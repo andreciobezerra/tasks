b33886ce379d6bd584f5929bf4f8b6fedc982d94
Andrecio Bezerra <andreciobezerra@gmail.com>
2024-09-13 18:18:10 -0300

    fixers

--- src/configs/mikro-orm.config.ts
+++ src/configs/mikro-orm.config.ts
@@ -1,3 +1,4 @@
+import { defineConfig } from "@mikro-orm/core";
 import { EntityGenerator } from "@mikro-orm/entity-generator";
 import { Migrator } from "@mikro-orm/migrations";
 import { MikroOrmModuleAsyncOptions } from "@mikro-orm/nestjs/typings";
@@ -5,8 +6,11 @@ import { PostgreSqlDriver } from "@mikro-orm/postgresql";
 import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 import { SqlHighlighter } from "@mikro-orm/sql-highlighter";
 import { ConfigService } from "@nestjs/config";
+import { config } from "dotenv";
 
-const defaultMikroOrmConfig: MikroOrmModuleAsyncOptions = {
+config({ path: "dev-env/.env.development" });
+
+export const defaultMikroOrmConfig: MikroOrmModuleAsyncOptions = {
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => ({
     host: configService.get("DB_HOST"),
@@ -17,6 +21,9 @@ const defaultMikroOrmConfig: MikroOrmModuleAsyncOptions = {
     autoLoadEntities: true,
     entities: ["dist/**/*.entity.js"],
     entitiesTs: ["src/**/*.entity.ts"],
+    migrations: {
+      pathTs: "src/database/migrations",
+    },
     debug: true,
     highlighter: new SqlHighlighter(),
     metadataProvider: TsMorphMetadataProvider,
@@ -26,4 +33,18 @@ const defaultMikroOrmConfig: MikroOrmModuleAsyncOptions = {
   }),
 };
 
-export default defaultMikroOrmConfig;
+const cliMikroOrmConfig = defineConfig({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  dbName: process.env.DB_NAME,
+  entities: ["dist/**/*.entity.js"],
+  migrations: {
+    pathTs: "src/database/migrations",
+  },
+  extensions: [Migrator],
+  driver: PostgreSqlDriver,
+});
+
+export default cliMikroOrmConfig;
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,6 @@
 import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Module } from "@nestjs/common";
-import defaultMikroOrmConfig from "src/configs/mikro-orm.config";
+import { defaultMikroOrmConfig } from "src/configs/mikro-orm.config";
 
 @Module({
   imports: [MikroOrmModule.forRootAsync(defaultMikroOrmConfig)],
