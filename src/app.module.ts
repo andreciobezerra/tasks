18c9fb19a3502ecc9973f26098068ae43032d31b
Andrecio Bezerra <andreciobezerra@gmail.com>
2024-09-13 18:20:05 -0300

    add task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from "./database/database.module";
+import { TaskModule } from "./task/task.module";
 import appConfig from "./configs/app.config";
 
 @Module({
-  imports: [ConfigModule.forRoot(appConfig), DatabaseModule],
+  imports: [ConfigModule.forRoot(appConfig), DatabaseModule, TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
